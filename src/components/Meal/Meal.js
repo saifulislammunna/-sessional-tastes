d871eebeb27b7c87652f4064f3e2cf5038dcac8f
saifulislammunna <saifulmunna28@gmail.com>
2021-09-25 20:58:20 +0600

    data-loaded

--- src/components/Meal/Meal.js
+++ src/components/Meal/Meal.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Meal.css'
+
+ 
 const Meal = () => {
+    const [products, setProducts] = useState([]);
+  useEffect(() => {
+      
+      fetch( 'https://www.themealdb.com/api/json/v1/1/search.php?s')
+      .then(res => res.json())
+      .then(data => setProducts(data.meals))
+  },[])
     return (
         <div className="meal-container">
             <div className="food-container">
-                <h2>Food:</h2> 
+                <h2>Food:{products.length} </h2> 
+                 {
+                     products.map(product => console.log(product))
+                 }
             </div>
             <div className="cart-container">
                 <h3>Order Summary  </h3>
